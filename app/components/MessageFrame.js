a41982255d8108ba7bd3e2dddbfc86edb022b0e9
ogbk <ogbu.k.olu@gmail.com>
2019-02-22 12:24:28 +0000

    <MessageFrame/> is now functional component

--- app/components/MessageFrame.js
+++ app/components/MessageFrame.js
@@ -1,85 +1,80 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class MessageFrame extends Component {
-  componentDidMount() { }
+export const MessageFrame = ({
+  displayMessage,
+  updateTracks,
+  reSelectAudio,
+  clearTracks,
+  showScreen,
+}) => {
+  let toRender = null;
 
-  render() {
-    const {
-      displayMessage: propsDisplayMessage,
-      updateTracks: propsUpdateTracks,
-      reSelectAudio: propsReSelectAudio,
-      clearTracks: propsClearTracks,
-      showScreen: propsShowScreen,
-    } = this.props;
-
-    let toRender = null;
-
-    switch (propsDisplayMessage) {
-      case 'NEWTRACK_FIRST_OR_LAST':
-        toRender = (
-          <div className="upload-options">
-            <div
-              className="frame-option click"
-              onClick={() => { propsUpdateTracks('add_first', null); }}
-            >
-              <span>Save as first track</span>
-            </div>
-
-            <div
-              className="frame-option click"
-              onClick={() => { propsUpdateTracks('add_last', null); }}
-            >
-              <span>Save as last track</span>
-            </div>
+  switch (displayMessage) {
+    case 'NEWTRACK_FIRST_OR_LAST':
+      toRender = (
+        <div className="upload-options">
+          <div
+            className="frame-option click"
+            onClick={() => { updateTracks('add_first', null); }}
+          >
+            <span>Save as first track</span>
           </div>
-        );
-        break;
 
-      case 'CONFIRM_CLEAR_TRACKS':
-        toRender = (
-          <div className="upload-options">
-            <div
-              className="frame-option click"
-              onClick={() => { propsClearTracks(); }}
-            >
-              <span>Clearing tracks. Continue?</span>
-            </div>
+          <div
+            className="frame-option click"
+            onClick={() => { updateTracks('add_last', null); }}
+          >
+            <span>Save as last track</span>
           </div>
-        );
-        break;
-
-      case 'NOT_AUDIO_FILE':
-        toRender = (
-          <div className="audio-error">
-            <div
-              className="frame-option click"
-              onClick={() => { propsReSelectAudio(); }}
-            >
-              <span>Please select only audio files</span>
-            </div>
-          </div>
-        );
-        break;
-
-      default:
-        break;
-    }
-
-    return (
-      <div>
-        {toRender}
-
-        <div
-          className="frame-abort click"
-          onClick={() => { propsShowScreen(true); }}
-        >
-          <span>ABORT</span>
         </div>
-      </div>
-    );
+      );
+      break;
+
+    case 'CONFIRM_CLEAR_TRACKS':
+      toRender = (
+        <div className="upload-options">
+          <div
+            className="frame-option click"
+            onClick={() => { clearTracks(); }}
+          >
+            <span>Clearing tracks. Continue?</span>
+          </div>
+        </div>
+      );
+      break;
+
+    case 'NOT_AUDIO_FILE':
+      toRender = (
+        <div className="audio-error">
+          <div
+            className="frame-option click"
+            onClick={() => { reSelectAudio(); }}
+          >
+            <span>Please select only audio files</span>
+          </div>
+        </div>
+      );
+      break;
+
+    default:
+      break;
   }
-}
+
+  return (
+    <div>
+      {toRender}
+
+      <div
+        className="frame-abort click"
+        onClick={() => { showScreen(true); }}
+      >
+        <span>ABORT</span>
+      </div>
+    </div>
+  );
+};
+
 
 MessageFrame.propTypes = {
   displayMessage: PropTypes.string.isRequired,
