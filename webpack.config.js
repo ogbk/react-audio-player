545edff2499b4c9e543e2e4add6dd48d6d664fd9
ogbk <ogbu.k.olu@gmail.com>
2019-10-18 18:23:10 +0100

    config, fix sass file

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
-var webpack = require('webpack');
+// var webpack = require('webpack');
 
 module.exports = {
-	entry: __dirname+"/app/components/index.js",
-	output:{
-		path: __dirname+"/public", filename: 'bundle.js'
-	},
-	module:{
-		loaders: [
-			{	test: /\.js$/, loader: 'babel-loader', exclude: /(node_modules)/ },
-      { test: /\.sass$/, loader: 'style-loader!css-loader!sass-loader' }
-		
-		]
-	}
-}
+  entry: `${__dirname}/app/components/index.js`,
+  output: {
+    path: `${__dirname}/public`,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader', exclude: /(node_modules)/ },
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+      { test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.(txt|jl)$/i, use: 'raw-loader' },
+    ],
+  },
+};
